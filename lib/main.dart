13f195edd272926b1aea9cd518ef8d4f32534012
AndroidDeveloper3005 <androdevhimel005@gmail.com>
2022-02-07 13:28:22 +0600

    Adding Images, Google Fonts | ElevatedButton | Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thirty_days_program/pages/homePage.dart';
 import 'package:thirty_days_program/pages/loginPage.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: const Home_Pages(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+         //primaryTextTheme: GoogleFonts.latoTextTheme()
+          ),
       //themeMode: ThemeMode.light,
       darkTheme: ThemeData(primarySwatch: Colors.red),
       //route
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -6,15 +6,48 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 28,
-              color: Colors.blueAccent,
-              fontWeight: FontWeight.bold),
-        ),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/images.png",
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 28.0),
+          const Text(
+            "Welcome to 30 Days of Flutter",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          const SizedBox(height: 28.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            //padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Enter Username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                ),
+                const SizedBox(height: 28.0),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Hi Himel");
+                  },
+                  child: const Text("Login"),
+                  style: TextButton.styleFrom(),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
