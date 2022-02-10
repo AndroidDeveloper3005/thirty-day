92e7439d652fb2b17a4ef5cdc3a177abf51bfa35
AndroidDeveloper3005 <androdevhimel005@gmail.com>
2022-02-10 22:19:32 +0600

    Models | AppBarTheme | Extracting Theme

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String price;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.image});
+}
+
+
+
