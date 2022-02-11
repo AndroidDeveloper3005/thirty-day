9e869d43f3b4278f8a561393a9106c24a2cb92f5
AndroidDeveloper3005 <androdevhimel005@gmail.com>
2022-02-11 14:10:43 +0600

    Local Files | Load & Decode JSON

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:thirty_days_program/models/catalog.dart';
 import 'package:thirty_days_program/widgets/ItemWidget.dart';
 import 'package:thirty_days_program/widgets/drawer.dart';
 
-// ignore: camel_case_types
-class Home_Pages extends StatelessWidget {
+class Home_Pages extends StatefulWidget {
   const Home_Pages({Key? key}) : super(key: key);
+
+  @override
+  State<Home_Pages> createState() => _Home_PagesState();
+}
+
+class _Home_PagesState extends State<Home_Pages> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJSON = await rootBundle.loadString("assets/files/products.json");
+    var decodedData = jsonDecode(catalogJSON);
+    var productData = decodedData["products"];
+    //print(decodedData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +38,7 @@ class Home_Pages extends StatelessWidget {
         itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
           return ItemWidget(
-            items:  CatalogModel.items[index],
+            items: CatalogModel.items[index],
           );
         },
       ),
